ee74afa7804e315019a4ebff33b90a5663e4500e
gonzaloruiz3 <gonirr3@hotmail.com>
2022-12-20 11:28:58 +0100

    imagienes para añadir

--- gonzalo-portfolio/src/components/Ejemplo.vue
+++ gonzalo-portfolio/src/components/Ejemplo.vue
@@ -1,19 +1,7 @@
 <template>
-<<<<<<< HEAD
-   <nav class="menu">
-  <ul>
-    <li><a href="#">Project</a></li>
-    <li><a href="#">CV</a></li>
-    <li><a href="#">Contact</a></li>
-    <li><a href="#">About me</a></li>
-  </ul>
-</nav>
-<button @click="cambiarMensaje"> Click me! </button>
-=======
  <div>
     <p>Ejemplo</p>
 
->>>>>>> d251d65742ee4ba37ab49457c9bac4e48c678a3a
 {{mensaje}}
 
 
--- gonzalo-portfolio/src/router.js
+++ gonzalo-portfolio/src/router.js
@@ -2,11 +2,13 @@ import * as VueRouter from "vue-router"
 
 import Ejemplo from "./components/Ejemplo.vue";
 import HelloWorld from "./components/HelloWorld.vue"
+import Projects from "./components/Projects.vue"
 
 
 export const routes = [
     { path: '/', name: "Home",  component: HelloWorld },
-    { path: '/example', name: "Example",  component: Ejemplo }
+    { path: '/example', name: "Example",  component: Ejemplo },
+    { path: '/cabeza', name: "Projects", component: Projects } 
 ]
 
 const router = VueRouter.createRouter({
